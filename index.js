454e5f90f64d688b987e0654e6276bfb6b9e5a2b
Nathaniel Richards <Legendarycomedy1@gmail.com>
2017-03-30 07:04:51 -0700

    Commit
    Last night

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const {app, BrowserWindow} = electron
 app.on('ready', () =>
 {
   let win = new BrowserWindow({width:800, height:600})
-  win.loadURL('file://' + __dirname + '/index.html')﻿
+  win.loadURL(`file://${__dirname}/index.html`)
 })
+
+exports.openWindow = (filename) =>
+{
+  let win = new BrowserWindow({width:800, height:600})
+  win.loadURL(`file://${__dirname}/` + filename + `.html`)
+}
